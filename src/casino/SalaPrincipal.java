10bd7979376043f41f21ec01acdf65346b7f1286
Pedro Perez <pedropm.lolo05@gmail.com>
2024-12-04 13:46:35 +0100

    librerias

--- src/casino/SalaPrincipal.java
+++ src/casino/SalaPrincipal.java
@@ -1,4 +1,4 @@
-package casino;
+ package casino;
 
 import java.io.IOException;
 import java.util.ArrayList;
--- src/juegos/Ruleta.java
+++ src/juegos/Ruleta.java
@@ -63,15 +63,15 @@ public class Ruleta extends Juego {
         System.out.println("----------------------------");
         System.out.println("_________________________________________________________________________");
         System.out.println("|     | 3 | 6 | 9 | 12 | 15 | 18 | 21 | 24 | 27 | 30 | 33 | 36 | 2 to 1 |");
-        System.out.println("|     -------------------------------------------------------------------");
+        System.out.println("|     |-----------------------------------------------------------------|");
         System.out.println("|     | 2 | 5 | 8 | 11 | 14 | 17 | 20 | 23 | 26 | 29 | 32 | 35 | 2 to 1 |");
-        System.out.println("|     -------------------------------------------------------------------");
+        System.out.println("|     |-----------------------------------------------------------------|");
         System.out.println("|  0  | 1 | 4 | 7 | 10 | 13 | 16 | 19 | 22 | 25 | 28 | 31 | 34 | 2 to 1 |");
-        System.out.println("|     -------------------------------------------------------------------");
-        System.out.println("|     |       1st 12        |        2nd 12     |         3rd 12        |");
-        System.out.println("|     -------------------------------------------------------------------");
+        System.out.println("|     |-----------------------------------------------------------------|");
+        System.out.println("|     |       1st 12        |       2nd 12      |         3rd 12        |");
+        System.out.println("|     |-----------------------------------------------------------------|");
         System.out.println("|     |  1 to 18  |   PAR   |   RED   |  BLACK  |  IMPAR  |   19 to 36  |");
-        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("|_____|_________________________________________________________________|");
     }
 
     // Mostrar los porcentages de ganancia
@@ -79,15 +79,15 @@ public class Ruleta extends Juego {
         System.out.println("__________________________ CHEET SHEET __________________________");
         System.out.println("| Tipo de Apuesta         | Ejemplo        | Pago               |");
         System.out.println("|-------------------------|----------------|--------------------|");
-        System.out.println("| Número Individual       | 7              | 35 a 1             |");
-        System.out.println("| Apuesta Split           | 7 y 8          | 17 a 1             |");
-        System.out.println("| Apuesta Street          | 7, 8, 9        | 11 a 1             |");
-        System.out.println("| Apuesta Corner          | 7, 8, 10, 11   | 8 a 1              |");
-        System.out.println("| Docena (1st, 2nd, 3rd)  | 1st 12         | 2 a 1              |");
-        System.out.println("| Columna                 | Columna 1      | 2 a 1              |");
-        System.out.println("| Mitad (1-18 o 19-36)    | 1-18           | 1 a 1              |");
-        System.out.println("| Rojo / Negro            | Rojo           | 1 a 1              |");
-        System.out.println("| Par / Impar             | Par            | 1 a 1              |");
+        System.out.println("| Número Individual       | 7              | x35                |");
+        System.out.println("| Apuesta Split           | 7 y 8          | x17                |");
+        System.out.println("| Apuesta Street          | 7, 8, 9        | x11                |");
+        System.out.println("| Apuesta Corner          | 7, 8, 10, 11   | x8                 |");
+        System.out.println("| Docena (1st, 2nd, 3rd)  | 1st 12         | x2                 |");
+        System.out.println("| Columna                 | Columna 1      | x2                 |");
+        System.out.println("| Mitad (1-18 o 19-36)    | 1-18           | x1                 |");
+        System.out.println("| Rojo / Negro            | Rojo           | x1                 |");
+        System.out.println("| Par / Impar             | Par            | x1                 |");
         System.out.println("|_______________________________________________________________|");
     }
     
